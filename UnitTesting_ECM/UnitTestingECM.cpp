99c05f1c6b2ead45578c3ffc5278200137767d33
Kevin Boylan <idakev2396@gmail.com>
2017-12-11 05:10:11 +0000

    Work from last session not successfully committed as file size was too large. Working backwards.

---
+++ UnitTesting_ECM/UnitTestingECM.cpp
@@ -0,0 +1,14 @@
+// UnitTestingECM.cpp : Defines the entry point for the console application.
+//
+
+#include "gtest\gtest.h"
+#include "RenderSystem.h"
+
+
+TEST(PotatoTest, Potatatat)
+{
+	RenderSystem rd;
+	
+	EXPECT_EQ(true, true);
+}
+

77a431bf11e66cc3a0d9105c8e3e016df1f315dd
Kevin Boylan <idakev2396@gmail.com>
2018-01-18 09:53:26 +0000

    Small changes.

--- Processes/Processes/Source.cpp
+++ Processes/Processes/Source.cpp
@@ -2,18 +2,21 @@
 #include <windows.h>
 #include <stdio.h>
 #include <tchar.h>
+#include <cstring>
 
 using namespace std;
 
 
-void Coordinator(int argc, TCHAR *argv[])
+void Coordinator(char cmdLine, TCHAR *argv[])
 {
 	//string path = "";
 	STARTUPINFO info = { sizeof(info) };
 	PROCESS_INFORMATION processInfo;
 
+	int nStrBuffer = cmdLine.getLength();
+
 	// Pass in path to Worker class
-	if (CreateProcess(path, command, NULL, NULL, TRUE, 0, NULL, NULL, &info, &processInfo))
+	if (CreateProcess(path,  cmdLine.GetBuffer(nStrBuffer), NULL, NULL, TRUE, 0, NULL, NULL, &info, &processInfo))
 	{
 		WaitForSingleObject(processInfo.hProcess, INFINITE);
 		CloseHandle(processInfo.hProcess);

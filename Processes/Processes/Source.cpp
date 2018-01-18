8e2615fd6d4101b98f0a9646d4a33ced86f9f96c
Kevin Boylan <idakev2396@gmail.com>
2018-01-18 09:44:17 +0000

    Started working on Coordinator.

--- Processes/Processes/Source.cpp
+++ Processes/Processes/Source.cpp
@@ -1,6 +1,29 @@
 #include <iostream>
+#include <windows.h>
+#include <stdio.h>
+#include <tchar.h>
 
 using namespace std;
+
+
+void Coordinator(int argc, TCHAR *argv[])
+{
+	//string path = "";
+	STARTUPINFO info = { sizeof(info) };
+	PROCESS_INFORMATION processInfo;
+
+	// Pass in path to Worker class
+	if (CreateProcess(path, command, NULL, NULL, TRUE, 0, NULL, NULL, &info, &processInfo))
+	{
+		WaitForSingleObject(processInfo.hProcess, INFINITE);
+		CloseHandle(processInfo.hProcess);
+		CloseHandle(processInfo.hThread);
+	}
+
+
+
+}
+
 
 int main()
 {

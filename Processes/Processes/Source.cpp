7fe79531f11de10fe3f4716692e179ddd255a2d3
Kevin Boylan <idakev2396@gmail.com>
2018-01-18 10:13:29 +0000

    Final changes.

--- Processes/Processes/Source.cpp
+++ Processes/Processes/Source.cpp
@@ -6,23 +6,50 @@
 
 using namespace std;
 
+#define TEXT
 
-void Coordinator(char cmdLine, TCHAR *argv[])
+
+void Coordinator()
 {
-	//string path = "";
-	STARTUPINFO info = { sizeof(info) };
+	string path = ""; // add in once Worker class is added 
+
+	// Specifies the window station, desktop, standard handles, and appearance of the main window for a process at creation time.
+	// [in] parameter
+	STARTUPINFO info;
+
+	// Fills a block of memory with zeros.
+	ZeroMemory(&info, sizeof(info));
+	info.cb = sizeof info; //Only compulsory field
+
+
+	// PROCESS_INFORMATION structure - receives identification information about the new process.
+	// what we get as an[out] parameter
 	PROCESS_INFORMATION processInfo;
-
-	int nStrBuffer = cmdLine.getLength();
-
+	ZeroMemory(&processInfo, sizeof(processInfo));
+ 
 	// Pass in path to Worker class
-	if (CreateProcess(path,  cmdLine.GetBuffer(nStrBuffer), NULL, NULL, TRUE, 0, NULL, NULL, &info, &processInfo))
+	if (CreateProcess(TEXT(path), NULL,
+		NULL, NULL, FALSE, CREATE_NEW_CONSOLE, NULL,
+		NULL, &info, &processInfo))
 	{
-		WaitForSingleObject(processInfo.hProcess, INFINITE);
-		CloseHandle(processInfo.hProcess);
-		CloseHandle(processInfo.hThread);
+		
 	}
 
+
+	// matrices
+	double a[3][3] = 
+	   { 8, 7, 9,
+		 5, 8, 4,
+		 9, 4, 6 };
+
+
+	double b[3][3] = 
+	  { 4, 9, 5,
+		8, 9, 5,
+		5, 6, 4 };
+
+
+	double c[3][3];
 
 
 }
@@ -30,5 +57,5 @@ void Coordinator(char cmdLine, TCHAR *argv[])
 
 int main()
 {
-
+	system("PAUSE");
 }
